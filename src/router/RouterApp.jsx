298c1b19ecdcac6ab8f06b8a59fa23ba0c43be7a
miguel-castillo-hub <consultores.castillo@gmail.com>
2025-07-22 16:55:23 -0300

    add: RouterApp casi terminado

---
+++ src/router/RouterApp.jsx
@@ -0,0 +1,17 @@
+import { Browser, Routes, Route } from "react-router-dom"
+
+const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export { RouterApp }
